6fe5e1e9bb606da5936036267304fde7349c85ed
theManITMask <albinoslime@gmail.com>
2019-02-06 12:07:58 +0000

    setup

---
+++ CrowCount/src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {	
+		Parent root;
+
+			try {
+				root = FXMLLoader.load(getClass().getResource("interface.fxml"));
+				 Scene scene = new Scene(root, 300, 275);
+			        primaryStage.setScene(scene);
+			        primaryStage.show();    
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		    
+		       
+		    }
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
