2635dafc8f364b679c24377da945d32a578967c3
20080120 <20080120@IT220PC09.wit.ie>
2019-02-27 11:36:59 +0000

    flatten function untested

--- CrowCount/src/application/flock.java
+++ CrowCount/src/application/flock.java
@@ -8,38 +8,38 @@ public class flock {
 	static ArrayList<String> keyList;
 
 	public static void addPixel(int x, int y) {
-		crowPixel newPixel = new crowPixel(x,y);
+		crowPixel newPixel = new crowPixel(x,y);// creats a new crow pixel
 		String key = ("x"+x+"y"+y).toString();
-		System.out.println(key);
-		crowDisjointSet.put(key, newPixel);
-		keyList.add(key);
+		System.out.println(key);//generats a hash to use
+		crowDisjointSet.put(key, newPixel);// adds the pixel to the hash table
+		keyList.add(key);// adds the flocks list of keys
 		//checkUp(newPixel);
 		//checkLeft(newPixel);
-		addLinks(newPixel);
+		addLinks(newPixel);//trys to link the pixel into the set
 		
 	}
 	
 	static private void addLinks(crowPixel pixel) {
-		crowPixel check;
-		String upKey = ("x"+(pixel.pixelX)+"y"+(pixel.pixelY-1)).toString();
-		String leftKey = ("x"+(pixel.pixelX-1)+"y"+(pixel.pixelY)).toString();
-		if(crowDisjointSet.get(upKey) != null) {
-			check = crowDisjointSet.get(upKey);
-			if(check.parent == null) {
+		crowPixel check;//stors the curent pixel being cheked for linkage
+		String upKey = ("x"+(pixel.pixelX)+"y"+(pixel.pixelY-1)).toString();// figers out what a pixel aboves key whould be
+		String leftKey = ("x"+(pixel.pixelX-1)+"y"+(pixel.pixelY)).toString();//figers out what a pixel aboves key whould be
+		if(crowDisjointSet.get(upKey) != null) {//if there is a usabule pixel above
+			check = crowDisjointSet.get(upKey);// sets check to the above pixel
+			if(check.parent == null) { //if the above pixel needs a perent the curent pixel becomes its perent otherwise it becomes the curent pixels perent
 				check.parent = pixel;
 			}
 			else {
 				pixel.parent = check;
 			}
 			
-			if(crowDisjointSet.get(leftKey) != null) {
+			if(crowDisjointSet.get(leftKey) != null) { //if the pixel to the left needs a perent the curent pixel becomes its perent
 				check = crowDisjointSet.get(leftKey);
 				if(check.parent == null) {
 					check.parent = pixel;
 				}
 			}
 		}
-		else if(crowDisjointSet.get(leftKey) != null) {
+		else if(crowDisjointSet.get(leftKey) != null) {// if there is no pixel above it dose the same but for the left pixel insted
 			check = crowDisjointSet.get(leftKey);
 			if(check.parent == null) {
 				check.parent = pixel;
@@ -55,11 +55,25 @@ public class flock {
 	//	Hashtable<String, crowPixel > test =crowDisjointSet;
 		int crowCount = 0;
 		for(String key: keyList) {
-			if(crowDisjointSet.get(key).parent==null) {
+			if(crowDisjointSet.get(key).parent==null) {//for every root pixel the crow count is incramented
 			crowCount++;	
 			}
 		}
 		return crowCount;
 		
+	}
+	
+	public static void flaten() {
+		for (String key: keyList) {
+			crowPixel current = crowDisjointSet.get(key);
+			crowPixel cheking;
+			if(current.parent != null) {
+				cheking = current.parent; 
+				while (cheking.parent!=null) {
+					cheking = cheking.parent;
+				}
+				current.parent = cheking;
+			}	
+		}	
 	}
 }
